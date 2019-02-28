03cf5535a4e2f147a2ecb4a3fcb3b8bec9d746a1
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2019-02-28 11:14:39 -0300

    ajusta formatters

--- helpers/formatters.js
+++ helpers/formatters.js
@@ -2,7 +2,7 @@ const buildPayload = require('../helpers/build-payload');
 const customTimes = require('./custom-times');
 const delorean = require('@gabrieluizramos/delorean-js');
 
-function formataPayload (data) {
+function formataPayload(data) {
     const weekDay = delorean.setDate(data).getWeekDay('short');
     const horarios = customTimes(weekDay);
     return buildPayload(data, horarios, weekDay);
@@ -10,7 +10,13 @@ function formataPayload (data) {
 
 const formatNumberWith2Digits = (number) => number < 10 ? `0${number}` : number;
 
+const formatDateToBr = (date) => `${formatNumberWith2Digits(date.getDate())}/${formatNumberWith2Digits(date.getMonth() + 1)}/${date.getFullYear()}`;
+
+const formateDateToISO = (date) => `${date.getFullYear()}-${formatNumberWith2Digits(date.getMonth() + 1)}-${formatNumberWith2Digits(date.getDate())}`;
+
 module.exports = {
     formataPayload,
-    formatNumberWith2Digits
+    formatNumberWith2Digits,
+    formatDateToBr,
+    formateDateToISO
 };

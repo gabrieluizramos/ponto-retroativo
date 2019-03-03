65a7c04e6d5a3ba437690e7ab2f6eea379a23797
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2019-03-02 21:24:49 -0300

    cria arquivo de configuracao para calendario

--- helpers/feriados-municipais/config.js
+++ helpers/feriados-municipais/config.js
@@ -1,7 +1,9 @@
-const DOMINIO_API = 'https://api.calendario.com.br/';
-const TOKEN = `token=${process.env.token || 'Z2FicmllbC5sdWl6LnJhbW9zQGdtYWlsLmNvbSZoYXNoPTIyMzMyMTE1OQ'}`;
-const CIDADE = `cidade=${process.env.cidade || 'SAO_PAULO'}`;
-const ESTADO = `estado=${process.env.estado || 'SP'}`;
+const calendarioConfig = require('../../config/calendario.json');
+
+const DOMINIO_API = calendarioConfig.url;
+const TOKEN = `token=${process.env.token || calendarioConfig.token}`;
+const CIDADE = `cidade=${process.env.cidade || calendarioConfig.cidade}`;
+const ESTADO = `estado=${process.env.estado || calendarioConfig.estado}`;
 const ANO = ano => `ano=${ano || new Date().getFullYear()}`;
 
 const API = ano => `${DOMINIO_API}?${ANO(ano)}&${ESTADO}&${CIDADE}&${TOKEN}`;
